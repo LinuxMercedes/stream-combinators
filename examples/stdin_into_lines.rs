4035f513697518cbe656965e338960d4755338f4
LinuxMercedes <LinuxMercedes@gmail.com>
2018-01-21 15:45:25 -0600

    FilterFold example

---
+++ examples/stdin_into_lines.rs
@@ -0,0 +1,50 @@
+extern crate futures;
+extern crate tokio_core;
+extern crate tokio_stdin;
+extern crate stream_combinators;
+
+use futures::stream::Stream;
+use tokio_core::reactor::Core;
+use tokio_stdin::spawn_stdin_stream_unbounded;
+use stream_combinators::FilterFoldStream;
+
+fn main() {
+    let mut core = Core::new().unwrap();
+
+    // Print stdin line-by-line
+    let prog = spawn_stdin_stream_unbounded()
+        .filter_fold(|buf, elem| {
+            match elem {
+                // Got a newline
+                Some(byte) if byte == '\n' as u8 => {
+                    let s = String::from_utf8(buf.to_vec()).unwrap();
+                    println!("debug stdin: {}", s);
+                    (vec![], Some(s))
+                },
+                // Got a byte
+                Some(byte) => {
+                    buf.push(byte);
+                    (buf.to_vec(), None)
+                },
+                // Got EOF
+                None => {
+                    if buf.len() > 0 {
+                        let s = String::from_utf8(buf.to_vec()).unwrap();
+                        println!("debug stdin is done: {}", s);
+                        (vec![], Some(s))
+                    }
+                    else {
+                        println!("debug stdin is done; no line");
+                        (buf.to_vec(), None)
+                    }
+                }
+            }
+        }, vec![])
+        .for_each(|line| {
+            println!("{}", line);
+            Ok(())
+        });
+
+
+    core.run(prog).unwrap()
+}
